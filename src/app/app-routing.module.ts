109571e96256ac59aaae1c73a405ca3a4a073a9c
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-04 08:03:47 +0530

    added initilized code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,3 +8,20 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+/*
+ 
+ If new component is created/instantiated , angular goes for couple of differet phases in this creation process. 
+ 
+          ngOnChanges => called after bound input property changes.
+          ngOnInit => called once after the component is initialized !
+          ngDoCheck => called during the every change detection done run
+          ngAfterContentInit => called after the content(ng-content) has been projected into view.
+          ngAfterContentChecked => called every time the projected content has been checked.
+          ngAfterViewInit => Called after component's view ( and child views ) has been initialized.
+          ngAfterViewChecked => called every time the view have been checked.
+          ngOnDestroy => called once the component is about to be destryed.
+   
+ */
